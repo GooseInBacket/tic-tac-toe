2667915facb53c4f6d055f451dba1540b37a7a9e
Dmitriy <dima951488@yanedx.ru>
2023-04-10 00:50:26 +0300

    add room existing exception

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from routers import game
@@ -13,6 +13,14 @@ templates = Jinja2Templates(directory='templates')
 
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    response = templates.TemplateResponse('index.html', {'request': request})
+    event, *key = request.url.query.split('=')
+    query = {
+        'name': 'index.html',
+        'status_code': 200,
+        'context': {'request': request}
+    }
+    if event == 'join' and not game.JOIN.get(*key):
+        query['name'] = 'room_error.html'
+        query['status_code'] = 404
+    response = templates.TemplateResponse(**query)
     return response
-
